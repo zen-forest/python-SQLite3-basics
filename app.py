c91bfbed021d29c999e5b09c9411a47210a34ba5
metasignal <timcollinsdesign+metasignal@gmail.com>
2022-12-18 14:24:24 -0500

    adding to the DB via an list of tuples

--- app.py
+++ app.py
@@ -10,5 +10,15 @@ cur.execute('''CREATE TABLE IF NOT EXISTS books(
   author text NOT NULL,
   price real);''')
 
+book_list = [
+  ('2','Animal Farm','George Orwell','7.49'),
+  ('3','The Power of Now','Eckhart Tolle','7.49'),
+  ('4','Mans Search For Meaning','Viktor Frankl','7.49'),
+  ('5','The Design of Everyday Things','Don Norman','7.49'),
+] 
+
+cur.executemany('''INSERT INTO books(id, title, author, price) values(?,?,?,?)
+''', book_list)
+
 db.commit()
-db.close() 
+db.close()  
